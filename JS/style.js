609b62bb53f33996330ab8952d76cf573f7368f7
Courtney Raphael <r.courtneygrace@gmail.com>
2020-02-09 23:20:14 -0500

    href entire cards

--- JS/style.js
+++ JS/style.js
@@ -1,11 +1,11 @@
-ScrollReveal().reveal('.card', {
-    delay: 275,
-    duration: 400,
-    reset:true
-});
+// ScrollReveal().reveal('.card', {
+//     delay: 275,
+//     duration: 400,
+//     reset:true
+// });
 
-ScrollReveal().reveal('.card2', {
-    delay: 275,
-    duration: 400,
-    reset:true
-});
+// ScrollReveal().reveal('.card2', {
+//     delay: 275,
+//     duration: 400,
+//     reset:true
+// });
